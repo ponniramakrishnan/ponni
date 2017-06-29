2aa13b57b810ff421c5f5b0979d3e7483ea347be
ponniramakrishnan <ponniramakrishna@gmail.com>
2017-06-29 14:59:26 +0530

    Create pnz.C

---
+++ pnz.C
@@ -0,0 +1,18 @@
+include<stdio.h>
+int main()
+{
+double a;
+printf("Enter the number");
+scanf("%lf",a);
+if(a>=0.0)
+  {
+    printf("number is positive");
+  }
+else if(a==0.0)
+  {
+    printf("number is zero");
+  }
+else
+  printf("number is positive");
+  
+}
