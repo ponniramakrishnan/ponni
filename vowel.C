8b31e82d4180225321d912fb6937f0da32e892f5
ponniramakrishnan <ponniramakrishna@gmail.com>
2017-06-29 15:11:00 +0530

    Create vowel.C

---
+++ vowel.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int l,u;
+printf("Enter the alphabet");
+scanf("%c",&c);
+l=(c==a ||c==e|| c==i || c==o|| c==u)
+u=(c==A ||c==E|| c==I || c==O|| c==U)
+
+if(l || u)
+{
+printf("Entered character is vowel");
+}
+else
+printf("Entered character is consonant");
+}
