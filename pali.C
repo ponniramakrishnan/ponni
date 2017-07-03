042793d1f4ac6520cc52dba4681b5cc094450df0
ponniramakrishnan <ponniramakrishna@gmail.com>
2017-07-03 14:19:01 +0530

    Create pali.C

---
+++ pali.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the number";
+scanf("%d",&a);
+n=a;
+while(a!=0)
+{
+b=a%10;
+c=c*10+b;
+a=a/10;
+}
+if(a=c)
+printf("number is palindrome");
+else
+printf("number is not palindrome");
+}
