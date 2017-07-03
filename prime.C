76edeef4971762446ed09dfdbbe05c2d931bfd47
ponniramakrishnan <ponniramakrishna@gmail.com>
2017-07-03 14:29:38 +0530

    Create prime.C

---
+++ prime.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter positive number");
+scanf("d%",a);
+for(i=2;i<=n/2;++i)
+{
+  if(n%i==0)
+  {
+  flag=1;
+  break;
+  }
+}
+if(flag==0)
+printf("Number is prime");
+else
+printf("number is not a prime");
+}
